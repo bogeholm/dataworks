6ccbc262e887f0bc26b78fc386c60107a0c39fed
bogeholm <troelsmikkelsen@gmail.com>
2020-02-18 15:09:33 +0100

    Update docs publishing method

--- generate-docs.sh
+++ generate-docs.sh
@@ -3,4 +3,8 @@
 # pip3 install pdoc3
 # https://github.com/pdoc3/pdoc
 # https://github.com/pdoc3/pdoc/issues/6
-pdoc --html -c show_type_annotations=True dataworks --output-dir public/
+pdoc --html --force -c show_type_annotations=True dataworks --output-dir docs
+rm -rf public/
+mkdir -p public/
+mv docs/dataworks/* public/
+touch public/.nojekyll
